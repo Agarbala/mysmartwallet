65ff4091a47645f49fab72941d1e490fcdd11388
Alessio <a.garofalobaladda@gmail.com>
2023-03-18 09:44:41 +0100

    first commit

---
+++ src/main/java/com/generation/mysmartwallet/MySmartWalletApplication.java
@@ -0,0 +1,13 @@
+package com.generation.mysmartwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySmartWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySmartWalletApplication.class, args);
+	}
+
+}
