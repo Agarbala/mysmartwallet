ae69e0271ddf418555de96417459302faa2d5e25
Alessio <a.garofalobaladda@gmail.com>
2023-03-21 08:50:42 +0100

    Aggiunto login.html e risolto un errore con la conf. dell'Interceptor

--- src/main/java/com/generation/mysmartwallet/config/WebConfig.java
+++ src/main/java/com/generation/mysmartwallet/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/logout");
+                .excludePathPatterns("/login", "/login.html", "/logout", "/error");
     }
 }
--- src/main/java/com/generation/mysmartwallet/interceptor/AuthInterceptor.java
+++ src/main/java/com/generation/mysmartwallet/interceptor/AuthInterceptor.java
@@ -10,17 +10,17 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HttpSession session = request.getSession();
-        if (session.getAttribute("user") == null) {
-            // L'utente non è autenticato, lo reindirizziamo alla pagina di login
-            response.sendRedirect("/login");
-            return false;
-        }
-        // L'autenticazione è valida, l'utente può accedere alla risorsa richiesta
-        return true;
-    }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			// L'utente non è autenticato, lo reindirizziamo alla pagina di login
+			response.sendRedirect("/login");
+			return false;
+		}
+		// L'autenticazione è valida, l'utente può accedere alla risorsa richiesta
+		return true;
+	}
 
 }
 
