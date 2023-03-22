9889482080ec6e5d801b5ce892b2ef6a9ada2f63
Alessio <a.garofalobaladda@gmail.com>
2023-03-22 15:47:40 +0100

    Aggiunti gli altri Bean a EntityContext

--- src/main/java/com/generation/mysmartwallet/config/EntityContext.java
+++ src/main/java/com/generation/mysmartwallet/config/EntityContext.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import com.generation.mysmartwallet.entity.Budget;
+import com.generation.mysmartwallet.entity.Obiettivo;
+import com.generation.mysmartwallet.entity.Transazione;
 import com.generation.mysmartwallet.entity.User;
 
 @Configuration
@@ -17,5 +20,29 @@ public class EntityContext {
 		User u = new User();
 		u.fromMap(row);
 		return u;
+	}
+	
+	@Bean
+	@Scope("prototype")
+	public Transazione transazione(Map<String, String> row) {
+		Transazione t = new Transazione();
+		t.fromMap(row);
+		return t;
+	}
+	
+	@Bean
+	@Scope("prototype")
+	public Obiettivo obiettivo(Map<String, String> row) {
+		Obiettivo o = new Obiettivo();
+		o.fromMap(row);
+		return o;
+	}
+	
+	@Bean
+	@Scope("prototype")
+	public Budget budget(Map<String, String> row) {
+		Budget b = new Budget();
+		b.fromMap(row);
+		return b;
 	}
 }
