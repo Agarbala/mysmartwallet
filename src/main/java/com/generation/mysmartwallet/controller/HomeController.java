19b9caca93e7c796b6af8c060835610c6b3b2d5c
AbdelbakyKarim <128139667+AbdelbakyKarim@users.noreply.github.com>
2023-03-24 10:11:18 +0100

    TASK Controllare

--- src/main/java/com/generation/mysmartwallet/controller/HomeController.java
+++ src/main/java/com/generation/mysmartwallet/controller/HomeController.java
@@ -1,17 +1,33 @@
 package com.generation.mysmartwallet.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.generation.mysmartwallet.entity.Conto;
+import com.generation.mysmartwallet.util.SessionUtil;
 
 
 @Controller
 public class HomeController {
 
+	
+	@Autowired
+	private ApplicationContext context;
+	
 	@GetMapping("/")
-	public String dashboard() {
-		
-		return "dashboard.jsp";
+	public String dashboard(Model model, HttpSession session) {
+		 model.addAttribute("conto", context.getBean(Conto.class, SessionUtil.idFromSession(session)).getId());
+		 	
+		 return "dashboard.jsp";
 	}
+	
+	
+	
 	
 	
 }
