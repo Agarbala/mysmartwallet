8cd6ae24de1b4894f125f548109b8c9eea3d5ed0
gianpieroferraro <gianpieroweno@hotmail.it>
2023-03-24 09:31:22 +0100

    rifiniture css, eliminato vecchio login e inserito script (che però non funziona per un problema poco comprensibile in console)

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,25 @@
-/**
- * 
- */
+//FORM LOGIN - occhio password nascondi/mostra
+
+iconaOcchio = document.querySelectorAll(".showHidePw");
+inputPass = document.querySelectorAll(".password");
+
+
+iconaOcchio.forEach(eyeIcon =>{
+    eyeIcon.addEventListener("click", function() {
+        inputPass.forEach(pwField =>{
+            if(pwField.type === "password") {
+                pwField.type = "text"
+                
+                iconaOcchio.forEach(icon =>{
+                    icon.classList.replace("uil-eye-slash", "uil-eye")
+                })
+            }else {
+                pwField.type = "password"
+                
+                iconaOcchio.forEach(icon =>{
+                    icon.classList.replace("uil-eye", "uil-eye-slash")
+                })
+            }
+        })
+    })
+}) 
